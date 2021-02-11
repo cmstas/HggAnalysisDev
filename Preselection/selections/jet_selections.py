2e14030593f7348d17257f366751ea0f0f398952
Samuel May <sammay@ucla.edu>
2021-02-10 16:31:47 -0800

    Add some jet related info to ttH leptonic (for training ttH leptonic BDT to check that we can roughly reproduce sensitivity)

--- Preselection/selections/jet_selections.py
+++ Preselection/selections/jet_selections.py
@@ -24,4 +24,21 @@ def select_jets(events, photons, electrons, muons, taus, options, debug):
 
     cut_diagnostics.add_cuts([pt_cut, eta_cut, dR_pho_cut, dR_ele_cut, dR_muon_cut, dR_tau_cut, jet_cut], ["pt > 25", "|eta| < 2.4", "dR_photons", "dR_electrons", "dR_muons", "dR_taus", "all"])
     
-    return jet_cut 
+    return jet_cut
+
+def set_jets(events, options, debug):
+    events["n_jets"] = awkward.num(events.Jet)
+
+    n_save = options["jets"]["n_jets_save"]
+    jet_pt_padded = utils.pad_awkward_array(events.Jet.pt, n_save, -9)
+    jet_eta_padded = utils.pad_awkward_array(events.Jet.eta, n_save, -9)
+    jet_id_padded = utils.pad_awkward_array(events.Jet.jetId, n_save, -9)
+    jet_btagDeepFlavB_padded = utils.pad_awkward_array(events.Jet.btagDeepFlavB, n_save, -9)
+
+    for i in range(n_save):
+        events["jet%s_pt" % str(i+1)] = jet_pt_padded[:,i]
+        events["jet%s_eta" % str(i+1)] = jet_eta_padded[:,i]
+        events["jet%s_id" % str(i+1)] = jet_id_padded[:,i]
+        events["jet%s_bTagDeepFlavB" % str(i+1)] = jet_btagDeepFlavB_padded[:,i]
+
+    return events
