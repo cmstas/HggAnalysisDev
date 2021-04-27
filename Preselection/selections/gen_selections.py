75aff932e6bcf39926390d7cde09e277ea04605d
Balaji Sathia Narayanan bsathian@ucsd.edu 7/6/2018 - Updated Changed Username <bsathian@uaf-10.t2.ucsd.edu>
2021-04-27 10:47:00 -0700

    update:

--- Preselection/selections/gen_selections.py
+++ Preselection/selections/gen_selections.py
@@ -1,4 +1,5 @@
 import awkward as ak
+import numpy as np
 
 def set_genZ(events, genBranches, selection_options, debug):
     if genBranches is None:
@@ -12,10 +13,15 @@ def set_genZ(events, genBranches, selection_options, debug):
         motherOfMuons = genBranches.genPartIdxMother[muon_idxs]
         motherOfTaus = genBranches.genPartIdxMother[tau_idxs]
 
-        ZToEleEvents = ak.sum((genBranches.pdgId[motherOfElectrons] == 23), axis=1) >= 2
-        ZToMuEvents = ak.sum((genBranches.pdgId[motherOfMuons] == 23), axis=1) >= 2
-        ZToTauEvents = ak.sum((genBranches.pdgId[motherOfTaus] == 23), axis=1) >= 2
-
-        events["genZ_decayMode"] = 1 * ZToEleEvents + 2 * ZToMuEvents + 3 * ZToTauEvents
-
+        ZToEleEvents = ak.sum((genBranches.pdgId[motherOfElectrons] == 23), axis=1) > 0 
+        ZToMuEvents = ak.sum((genBranches.pdgId[motherOfMuons] == 23), axis=1) > 0 
+        ZToTauEvents = ak.sum((genBranches.pdgId[motherOfTaus] == 23), axis=1) > 0 
+        
+        # W decay dudes
+        WToEEvents = ak.sum((abs(genBranches.pdgId[motherOfElectrons]) == 24), axis = 1) > 0
+        WToMuEvents = ak.sum((abs(genBranches.pdgId[motherOfMuons]) == 24), axis = 1) > 0
+        WToTauEvents = ak.sum((abs(genBranches.pdgId[motherOfTaus]) == 24), axis = 1) > 0
+        
+        events["genZ_decayMode"] = ak.from_numpy(np.zeros(len(events))) + 1 * ZToEleEvents + 2 * ZToMuEvents + 3 * ZToTauEvents + 4 * WToEEvents + 5 * WToMuEvents + 6 * WToTauEvents
+        events["tau_motherID"] = ak.fill_none(ak.firsts(genBranches.pdgId[motherOfTaus]),0)
     return events
