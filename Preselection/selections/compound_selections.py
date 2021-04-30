df2f909fb4d5ce36f00ec746a63605fd01cac511
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2021-04-29 18:33:46 -0700

    addressing change requests

--- Preselection/selections/compound_selections.py
+++ Preselection/selections/compound_selections.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy
 import awkward as ak
 import vector
 
--- Preselection/selections/gen_selections.py
+++ Preselection/selections/gen_selections.py
@@ -1,9 +1,9 @@
 import awkward as ak
-import numpy as np
+import numpy
 
 def set_genZ(events, genBranches, selection_options, debug):
     if genBranches is None:
-        events["genZ_decayMode"] = ak.from_numpy(-1 * np.ones(len(events)))
+        events["genZ_decayMode"] = ak.from_numpy(-1 * numpy.ones(len(events)))
     else:
         electron_idxs = abs(genBranches.pdgId) == 11
         muon_idxs = abs(genBranches.pdgId) == 13
@@ -13,15 +13,15 @@ def set_genZ(events, genBranches, selection_options, debug):
         motherOfMuons = genBranches.genPartIdxMother[muon_idxs]
         motherOfTaus = genBranches.genPartIdxMother[tau_idxs]
 
-        ZToEleEvents = ak.sum((genBranches.pdgId[motherOfElectrons] == 23), axis=1) > 0 
-        ZToMuEvents = ak.sum((genBranches.pdgId[motherOfMuons] == 23), axis=1) > 0 
-        ZToTauEvents = ak.sum((genBranches.pdgId[motherOfTaus] == 23), axis=1) > 0 
-        
+        ZToEleEvents = ak.sum((genBranches.pdgId[motherOfElectrons] == 23), axis=1) > 0
+        ZToMuEvents = ak.sum((genBranches.pdgId[motherOfMuons] == 23), axis=1) > 0
+        ZToTauEvents = ak.sum((genBranches.pdgId[motherOfTaus] == 23), axis=1) > 0
+
         # W decay dudes
         WToEEvents = ak.sum((abs(genBranches.pdgId[motherOfElectrons]) == 24), axis = 1) > 0
         WToMuEvents = ak.sum((abs(genBranches.pdgId[motherOfMuons]) == 24), axis = 1) > 0
         WToTauEvents = ak.sum((abs(genBranches.pdgId[motherOfTaus]) == 24), axis = 1) > 0
-        
-        events["genZ_decayMode"] = ak.from_numpy(np.zeros(len(events))) + 1 * ZToEleEvents + 2 * ZToMuEvents + 3 * ZToTauEvents + 4 * WToEEvents + 5 * WToMuEvents + 6 * WToTauEvents
+
+        events["genZ_decayMode"] = ak.from_numpy(numpy.zeros(len(events))) + 1 * ZToEleEvents + 2 * ZToMuEvents + 3 * ZToTauEvents + 4 * WToEEvents + 5 * WToMuEvents + 6 * WToTauEvents
         events["tau_motherID"] = ak.fill_none(ak.firsts(genBranches.pdgId[motherOfTaus]),0)
     return events
