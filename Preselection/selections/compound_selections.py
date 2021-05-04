fd4d5256d26b446b445e5fcf38805b23255be385
Balaji Sathia Narayanan bsathian@ucsd.edu 7/6/2018 - Updated Changed Username <bsathian@uaf-10.t2.ucsd.edu>
2021-05-03 19:15:59 -0700

    helicity variables

--- Preselection/selections/compound_selections.py
+++ Preselection/selections/compound_selections.py
@@ -15,12 +15,12 @@ def compound_selections(events, options, debug):
 
 def set_helicity_angles(events, taus, photons):
 
-    leadingTauVector = vector.awk({"pt": taus[:,0].pt, "eta": taus[:,0].eta, "phi": taus[:,0].phi, "mass": taus[:,0].mass})
+    leadingTauVector = vector.awk({"pt": events['tau1_pt'], "eta": events['tau1_eta'], "phi": events['tau1_phi'], "mass":events['tau1_mass']})
+
     SVFitVector = vector.awk({"pt": events["pt_tautauSVFitLoose"], "eta": events["eta_tautauSVFitLoose"], "phi": events["phi_tautauSVFitLoose"], "mass": events["m_tautauSVFitLoose"]})
     g1Vector = vector.awk({"pt": photons[:,0].pt, "eta": photons[:,0].eta, "phi": photons[:,0].phi, "mass": photons[:,0].mass})
     g2Vector = vector.awk({"pt": photons[:,1].pt, "eta": photons[:,1].eta, "phi": photons[:,1].phi, "mass": photons[:,1].mass})
     HggVector = g1Vector + g2Vector
-
     cosTheta = compute_helicity_angles(leadingTauVector, SVFitVector, HggVector)
     events["cos_theta_helicity"] = awkward.from_numpy(cosTheta)
 
@@ -30,18 +30,20 @@ def set_helicity_angles(events, taus, photons):
 @numba.njit
 def compute_helicity_angles(leadingTauVector, SVFitVector, HggVector):
     nEvents = len(leadingTauVector)
-    cosTheta = numpy.zeros(nEvents)
+    cosTheta = numpy.ones(nEvents, numpy.float64) * -9.0
     for i in range(nEvents):
         HTauTau = SVFitVector[i]
-        if HTauTau.pt < 0:
+        if HTauTau.mass < 0:
             cosTheta[i] = -9
             continue
         Hgg = HggVector[i]
         leadingTau = leadingTauVector[i]
-        triHVertex = HTauTau + Hgg
-        leadingTauInSVFitFrame = leadingTau.boost_p4(-HTauTau)
-        HTauTauInTriVertexFrame = HTauTau.boost_pr(-triHVertex)
-
-        cosTheta[i] = HTauTauInTriVertexFrame.dot(leadingTauInSVFitFrame)/(HTauTauInTriVertexFrame.mag * leadingTauInSVFitFrame.mag)
+        try:
+            triHVertex = HTauTau + Hgg
+            leadingTauInSVFitFrame = leadingTau.boost_p4(-HTauTau)
+            HTauTauInTriVertexFrame = HTauTau.boost_p4(-triHVertex)
+            cosTheta[i] = (HTauTauInTriVertexFrame.x * leadingTauInSVFitFrame.x + HTauTauInTriVertexFrame.y * leadingTauInSVFitFrame.y + HTauTauInTriVertexFrame.z * leadingTauInSVFitFrame.z)/(HTauTauInTriVertexFrame.mag * leadingTauInSVFitFrame.mag)
+        except:
+            cosTheta[i] = -9
 
     return cosTheta
